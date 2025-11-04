286522bd178aa10960c673280fb191089466bdf0
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-11-04 17:40:03 +0000

    refact: update web search node

--- app/nodes/news_scrapping_node.py
+++ app/nodes/news_scrapping_node.py
@@ -1,6 +1,11 @@
+from app.agents.state import AgentState
 from app.tools.web_search_tool import WebSearchTool
 
 
-def web_search_node(query: str) -> str:
+def web_search_node(state: AgentState) -> AgentState:
+    query = state["query"]
     tool = WebSearchTool()
-    return tool.run(query)
+    web_context = tool.run(query)
+    state["news_content"] = web_context
+
+    return state
