019fa0b89f861d2c82a6d543160ab3d7ed868ce3
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-11-07 23:08:28 +0000

    refact: improve summary analysis prompt

--- app/prompts/prompts.py
+++ app/prompts/prompts.py
@@ -4,5 +4,23 @@ from langchain_core.prompts import ChatPromptTemplate
 
 class PromptTemplates(Enum):
     REPORT_SUMMARY = ChatPromptTemplate.from_template(
-        "Generate a concise summary of the following healthcare report data: {data}"
+        """You are an assistant that interprets and explain healthcare data, and you must write a concise analysis of healthcare report data.
+        Context overview:
+        The context data is composed by two main categories:
+            - Metrics: pre-calculated SRAG metrics obtained by the provided data (e.g., mortality rate, cases variation rate, ICU variation rate, vaccination rate). "Variation" signifies a decline or increase.
+            - News: recent news articles about SRAG situation in the country.
+        Based on context provided, Your primary goal is to correlate the metrics with the News data. 
+        You must use the news to provide explanations and commentary on why the metrics are changing,
+        if there are any correlations available to be done.
+        you should also provide an outlook on the overall SRAG scene in the country, delighting how 
+        the situation is evolving, for better or worse, and the analytical conclusion regarding the cases.
+        
+        General Rules:
+        The analysis must be no longer than 2 paragraphs.
+        Base all explanations strictly on the provided context. Do not invent new numbers or events. 
+        However, you must draw logical connections between the news and the metrics to explain the overall scenario.
+        You must write the analysis in a analytical and authoritative tone.
+        Data:
+        To build the analysis, use the following data: {context_data}
+        """
     )
