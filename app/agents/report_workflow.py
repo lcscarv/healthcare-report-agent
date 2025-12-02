9752b2131829f9770fbc3a7b12eaa17dcf3f1d3e
Lucas Carvalho <lucas.carvalho@mesh-ai.com>
2025-12-02 16:32:07 +0000

    feature: update workflow with new nodes and concurrency between unrelated nodes

--- app/agents/report_workflow.py
+++ app/agents/report_workflow.py
@@ -1,25 +1,30 @@
 from langgraph.graph import StateGraph, START, END
 
 from app.agents.state import AgentState
-from app.nodes.summarizer_node import summmarizer_node
+from app.nodes.plot_data_node import get_plot_data_node
+from app.nodes.plot_data_insights_node import get_plot_insights_node
 from app.nodes.build_context_node import context_builder_node
-from app.nodes.data_load_node import data_loader_node
-from app.nodes.news_scrapping_node import web_search_node
 from app.nodes.metrics_node import metrics_node
+from app.nodes.news_scrapping_node import web_search_node
+from app.nodes.summarizer_node import summmarizer_node
 
 
 workflow = StateGraph(state_schema=AgentState)
 
-workflow.add_node("data_loader_node", data_loader_node)
 workflow.add_node("web_search_node", web_search_node)
 workflow.add_node("metrics_node", metrics_node)
+workflow.add_node("get_plot_data_node", get_plot_data_node)
+workflow.add_node("get_plot_insights_node", get_plot_insights_node)
 workflow.add_node("context_builder_node", context_builder_node)
 workflow.add_node("summarizer_node", summmarizer_node)
 
-workflow.add_edge(START, "data_loader_node")
-workflow.add_edge("data_loader_node", "metrics_node")
-workflow.add_edge("metrics_node", "web_search_node")
+workflow.add_edge(START, "metrics_node")
+workflow.add_edge(START, "web_search_node")
+workflow.add_edge(START, "get_plot_data_node")
+workflow.add_edge("get_plot_data_node", "get_plot_insights_node")
+workflow.add_edge("metrics_node", "context_builder_node")
 workflow.add_edge("web_search_node", "context_builder_node")
+workflow.add_edge("get_plot_insights_node", "context_builder_node")
 workflow.add_edge("context_builder_node", "summarizer_node")
 workflow.add_edge("summarizer_node", END)
 
