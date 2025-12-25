4ec736e280a6dea39fcc7012b8221269ce3a41a3
Lucas Carvalho <lucas.carvalho@mesh-ai.com>
2025-12-25 22:13:56 +0100

    feature: update summarizer node to check if there's any need for rewriting

--- app/nodes/summarizer_node.py
+++ app/nodes/summarizer_node.py
@@ -9,6 +9,15 @@ settings = load_settings()
 
 def summmarizer_node(state: AgentState) -> dict[str, AIMessage]:
     llm = state.llm
+    if state.iteration_count >= 1:
+        prompt = PromptTemplates.SUMMARY_REWRITE_WITH_FEEDBACK.value.format(
+            context_data=state.context,
+            original_summary=state.response.content,
+            feedback=state.feedback,
+        )
+        response = llm.invoke(prompt)
+        return {"response": response}
+
     prompt = PromptTemplates.REPORT_SUMMARY.value.format(context_data=state.context)
     response = llm.invoke(prompt)
     return {"response": response}
